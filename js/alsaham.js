49ee26ae19f1f93c3326ca33c9f272107efcc5d7
Ghost164t73 <151441449+Ghost164t73@users.noreply.github.com>
2023-12-14 18:02:15 +0100

    Update alsaham.js

--- js/alsaham.js
+++ js/alsaham.js
@@ -51,9 +51,17 @@ function fadeInOnScroll() {
   
 window.addEventListener('scroll', fadeInOnScroll);
 
-const cart = [];
 
-const subContainer = document.querySelector('.message-container .sub-container');
 
-console.log(subContainer)
-
+menuBtn.addEventListener('click', () => {
+    const sidebarWidth = sidebar.offsetWidth;
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = `-${sidebarWidth}px`;
+        content.style.marginLeft = '0';
+        menuBtn.innerHTML = '&#9776;';
+    } else {
+        sidebar.style.left = '0';
+        content.style.marginLeft = `${sidebarWidth}px`;
+        menuBtn.innerHTML = '&#10006;';
+    }
+});
